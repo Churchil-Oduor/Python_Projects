fec6a96491129117e2c202b912e27e9bf46d0108
Churchil-Oduor <churchilokechoduor@gmail.com>
2025-04-03 12:56:58 +0300

    defined the resize functions in a sepate file for modularity

--- open_cv/basics/access_camera.py
+++ open_cv/basics/access_camera.py
@@ -1,10 +1,14 @@
 import cv2 as cv
+import utils as util
+
 
 capture = cv.VideoCapture(0)
 
 while True:
     isTrue, frame = capture.read()
-    cv.imshow("video", frame)
+    resized_frame = util.video_resize(frame, 0.5)
+
+    cv.imshow("video", resized_frame)
 
     if cv.waitKey(20) and 0xff == ord('d'):
         break
--- open_cv/basics/resize_image.py
+++ open_cv/basics/resize_image.py
@@ -1,16 +1,11 @@
 import cv2 as cv
+import utils as util
 
-
-def resize(img, scale=0.75):
-    width = int(img.shape[0] * scale)
-    height = int(img.shape[1] * scale)
-    dimensions = (width, height)
-
-    return cv.resize(img, dimensions, interpolation=cv.INTER_AREA)
 
 
 img = cv.imread("photos/CAT.jpeg")
-resized_img = resize(img, 10)
+
+resized_img = util.img_resize(img, 10)
 
 cv.imshow('cat', resized_img)
 
